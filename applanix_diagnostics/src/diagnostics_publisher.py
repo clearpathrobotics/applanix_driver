2296b417a8ee5c1eb91ced7a5bddbe55dc87cc86
Administrator <administrator@cpr-que-01.(none)>
2012-05-11 14:40:44 -0400

    Node to republish status info on the diagnostics topic.

---
+++ applanix_diagnostics/src/diagnostics_publisher.py
@@ -0,0 +1,46 @@
+
+# ROS
+import rospy
+import applanix_msgs.msg
+from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
+
+
+class BitfieldRepublisher(object):
+  def __init__(self, topic_name, topic_type, fields):
+    flags = []
+    attrs = dir(applanix_msgs.msg.GeneralStatus)
+    for field in fields:
+      prefix = field.upper() + "_"
+      field_flags = []
+      for attr in attrs:
+        if attr.startswith(prefix):
+          short_name = attr[len(prefix):len(attr)]
+          mask = getattr(applanix_msgs.msg.GeneralStatus, attr)
+          field_flags.append((mask, short_name))
+      field_flags.sort()
+      flags.append((field, tuple(field_flags)))
+    self.flags = tuple(flags)
+
+    self.status_msg = DiagnosticStatus()
+    self.status_msg.level = DiagnosticStatus.OK
+    self.status_msg.name = "Applanix AP10"
+    self.status_msg.message = "OK"
+    rospy.Subscriber(topic_name, topic_type, self._cb)
+    self.pub = rospy.Publisher("/diagnostics", DiagnosticStatus, latch=True)
+
+  def _cb(self, msg):
+    self.status_msg.values = []
+    for field, field_flags in self.flags:
+      field_value = getattr(msg, field)
+      for mask, flag in field_flags:
+        value = str(int(field_value & mask != 0))
+        self.status_msg.values.append(KeyValue(flag, value))
+    self.pub.publish(self.status_msg) 
+
+
+def main():
+  rospy.init_node('applanix_diagnostics')
+  BitfieldRepublisher("status/general", applanix_msgs.msg.GeneralStatus, \
+                      ('status_a', 'status_b', 'status_c', 'fdir_1', \
+                       'fdir_2', 'fdir_3', 'fdir_4', 'fdir_5', 'extended'))
+  rospy.spin()
